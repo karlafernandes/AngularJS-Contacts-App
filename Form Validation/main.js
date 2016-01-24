4dc63fd8210b1ba5d330d06d96dbaf214287a9cd
Karla Fernandes <karla@vitaminak.com.br>
2016-01-24 18:44:44 +0200

    feat: Form Validation - Part 1

--- Form Validation/main.js
+++ Form Validation/main.js
@@ -3,15 +3,20 @@ var app = angular.module("minmax", []);
 app.controller("MinMaxCtrl", function ($scope, $http) {
     $scope.formModel = {};
 
-    $scope.onSubmit = function () {
-        console.log("Hey i"m submitted!");
-        console.log($scope.formModel);
+    $scope.onSubmit = function (valid) {
 
-        $http.post("https://minmax-server.herokuapp.com/register/", $scope.formModel).
-        success(function (data) {
-            console.log(":)")
-        }).error(function(data) {
-            console.log(":(")
-        });
+        if (valid) {
+            console.log("Hey i"m submitted!");
+            console.log($scope.formModel);
+
+            //$http.post("https://minmax-server.herokuapp.com/register/", $scope.formModel).
+            //	success(function (data) {
+            //		console.log(":)")
+            //	}).error(function(data) {
+            //		console.log(":(")
+            //	});
+        } else {
+            console.log("Form Invalid");
+        }
     };
 });
