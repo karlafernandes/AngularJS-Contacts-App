1f86f8f4d6c91247fb72802b1d0c0a459589ab84
Karla Fernandes <karla@vitaminak.com.br>
2016-01-24 21:25:49 +0200

    feat: filtering results

--- Contacts App/main.js
+++ Contacts App/main.js
@@ -2,6 +2,7 @@ var app = angular.module("codecraft", []);
 
 app.controller('PersonsController', function ($scope) {
 
+    $scope.search = {};
     $scope.selectedIndex = null;
     $scope.selectedPerson = null;
 
